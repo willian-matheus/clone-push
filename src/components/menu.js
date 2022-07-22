54e10d36b6a8a87fea83712d2d4f52f173c7d9bc
willian-matheus <109594242+willian-matheus@users.noreply.github.com>
2022-07-22 08:06:26 -0300

    .

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,24 @@
+import styled from "styled-components";
+
 export default function Menu() {
   return (
+    <>
     <div>
-      <h1>PUSH</h1>
+      <h1>Push - [ 2.22.1 - 27062022 ]</h1>
     </div>
+      <StyledMenu>
+        <li>Configurações</li>
+        <li>Cadastro</li>
+        <li>Processo</li>
+        <li>Consultas</li>
+        <li>Centificados</li>
+        <li>Ultilitario</li>
+        <li>Redes Sociais</li>
+      </StyledMenu>
+    </>
   );
 }
+
+const StyledMenu = styled.li`
+color:red;
+`;
