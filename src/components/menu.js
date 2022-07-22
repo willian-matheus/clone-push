372d0872ad0a166fd92da0d146139178e14a4e9a
willian-matheus <109594242+willian-matheus@users.noreply.github.com>
2022-07-22 17:57:37 -0300

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,13 +1,24 @@
 import styled from "styled-components";
+import MenuItem from "@material-ui/core/MenuItem";
 
 export default function Menu() {
+
+  const [click, setClick] = React.useState(null); 
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
   return (
+
+    
     <>
     <div>
       <h1>Push - [ 2.22.1 - 27062022 ]</h1>
     </div>
       <StyledMenu>
-        <li>Configurações</li>
+        <li
+        onClick={handleClick}>Configurações</li>
         <li>Cadastro</li>
         <li>Processo</li>
         <li>Consultas</li>
@@ -19,6 +30,12 @@ export default function Menu() {
   );
 }
 
-const StyledMenu = styled.li`
-color:red;
-`;
+const StyledMenu = styled.h4`
+color:black;
+display: flex;
+flex-wrap:wrap;
+justify-content:space-between;
+text-align: center;
+margin-top: 15px;
+background-color: red;
+`
