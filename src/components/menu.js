a585b9a1434a8e1dee11a83eb6ff4f1252c684a9
willian-matheus <109594242+willian-matheus@users.noreply.github.com>
2022-09-22 14:47:33 -0300

    .

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,42 +1,49 @@
 import "./menu.css";
+import React from "react";
 
 export default function Menu() {
   return (
     <>
-
-   <ul>
-
-   
-      <li> <a href="#">Configurações</a></li>
-      <li><a href="#">Configurações</a>
-
-
+      <nav className="dp-menu">
         <ul>
-          <li><a href="#">jogos</a></li>
-          <li><a href="#">jogos</a></li>
-          <li><a href="#">jogos</a>
-          </li><li><a href="#">jogos</a></li>
+          <li>
+            <a href="#">Principal</a>
+          </li>
+          <li>
+            <a href="#">Lojas</a>
+
+            <ul>
+              <li>
+                <a href="#">Text1</a>
+              </li>
+              <li>
+                <a href="#">Text2</a>
+              </li>
+              <li>
+                <a href="#">Text3</a>
+              </li>
+              <li>
+                <a href="#">Text4</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Eventos</a>
+          </li>
+          <li>
+            <a href="#">Vagas</a>
+          </li>
+          <li>
+            <a href="#">Rede Social</a>
+          </li>
+          <li>
+            <a href="#">Sobre</a>
+          </li>
+          <li>
+            <a href="#">Configurações</a>
+          </li>
         </ul>
-
-
-      </li>
-      <li>
-        <a href="#">Configurações</a>
-      </li>
-      <li>
-        <a href="#">Configurações</a>
-      </li>
-      <li>
-        <a href="#">Configurações</a>
-      </li>
-      <li>
-        <a href="#">Configurações</a>
-      </li>
-      <li>
-        <a href="#">Configurações</a>
-      </li>
-
-  </ul>
+      </nav>
     </>
   );
 }
